9d5ee7bf309915826eabda524ae295a6fc4dc9c8
Paulo Vieira de Lima <paulovlima@usp.br>
2022-11-16 17:38:53 -0300

    Adicionado tags

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,7 +1,8 @@
 from django.contrib import admin
-from .models import User, Cliente, Empresa
+from .models import User, Cliente, Empresa, Tags
 # Register your models here.
 
 admin.site.register(User)
 admin.site.register(Cliente)
 admin.site.register(Empresa)
+admin.site.register(Tags)
